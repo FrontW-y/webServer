6274bcd911699a98e2aa65f5eadb7a6e2b394de7
FrontW-y <75745504+FrontW-y@users.noreply.github.com>
2023-04-14 01:17:20 +0200

    Update webConfig.sh

--- webConfig.sh
+++ webConfig.sh
@@ -1,48 +1,53 @@
 #!/bin/bash
 
-echo "Mise à jour des paquets"
+# Couleur vert clair saturé
+GREEN="\e[1;32m"
+# Réinitialiser la couleur
+RESET="\e[0m"
+
+echo -e "${GREEN}Mise à jour des paquets${RESET}"
 apt-get update
 
-echo "Installation d'Apache"
+echo -e "${GREEN}Installation d'Apache${RESET}"
 apt-get install apache2 -y
 
-echo "Configuration du nom de la machine"
+echo -e "${GREEN}Configuration du nom de la machine${RESET}"
 hostnamectl set-hostname www
 
-echo "Création de l'utilisateur admin et configuration du mot de passe"
+echo -e "${GREEN}Création de l'utilisateur admin et configuration du mot de passe${RESET}"
 useradd -m -s /bin/bash admin
 echo "admin:vitrygtr" | chpasswd
 usermod -aG sudo admin
 
-echo "Création des répertoires nécessaires"
+echo -e "${GREEN}Création des répertoires nécessaires${RESET}"
 mkdir -p /var/www/html/images
 mkdir -p /var/www/html/private
 
-echo "Création des fichiers index.html et perdu.html"
+echo -e "${GREEN}Création des fichiers index.html et perdu.html${RESET}"
 bash -c 'echo "<h1>Bienvenue sur notre site !</h1>" > /var/www/html/index.html'
 bash -c 'echo "<h1>Traduction de Perdu.com</h1>" > /var/www/html/perdu.html'
 
-cp webServer/image.jpg /var/www/html/images/
+cp image.jpg /var/www/html/images/
 
-echo "Configuration de la protection par mot de passe du répertoire private"
+echo -e "${GREEN}Configuration de la protection par mot de passe du répertoire private${RESET}"
 htpasswd -c /etc/apache2/.htpasswd admin
 bash -c 'echo "AuthType Basic
 AuthName \"Accès restreint\"
 AuthUserFile /etc/apache2/.htpasswd
 Require valid-user" > /var/www/html/private/.htaccess'
 
-echo "Autoriser l'utilisation des fichiers .htaccess et activer le module rewrite"
+echo -e "${GREEN}Autoriser l'utilisation des fichiers .htaccess et activer le module rewrite${RESET}"
 sed -i '/<\/VirtualHost>/i <Directory "/var/www/html/private">\n    AllowOverride All\n<\/Directory>' /etc/apache2/sites-available/000-default.conf
 a2enmod rewrite
 
-echo "Configuration de la page d'erreur personnalisée"
+echo -e "${GREEN}Configuration de la page d'erreur personnalisée${RESET}"
 bash -c 'echo "ErrorDocument 404 /erreur.html" > /etc/apache2/conf-available/custom-errors.conf'
 bash -c 'echo "<h1>Désolé cette page nexiste pas !</h1>" > /var/www/html/erreur.html'
 a2enconf custom-errors
 
-echo "Redémarrage d'Apache"
+echo -e "${GREEN}Redémarrage d'Apache${RESET}"
 systemctl restart apache2
 
-echo "Le déploiement est terminé."
+echo -e "${GREEN}Le déploiement est terminé.${RESET}"
 
 systemctl status apache2
